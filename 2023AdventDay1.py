760b6d7f947bace159386852d3f85cfb0fd9b7b6
Steve <steevo83@gmail.com>
2023-12-01 16:04:41 -0500

    Add files via upload
    i just copied and pasted the doc to a .txt, didn't feel like trying to remember how to crawl a site. -- that's just part 1.

---
+++ 2023AdventDay1.py
@@ -0,0 +1,14 @@
+with open('Calibration document.txt') as f:
+    alllines = []
+    for line in f.readlines():
+        nums = []
+        for char in line:
+            if char.isnumeric():
+                nums.append(char)
+        print(nums)
+        linetot = nums[0]+nums[-1]
+        print(linetot)
+        alllines.append(linetot)
+    allnums = [int(x) for x in alllines]
+    alltot = sum(allnums)
+    print(alltot)
