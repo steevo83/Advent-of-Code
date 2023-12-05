1578ef5fd7477d8827bef6dae15f78827724cbcf
Steve <steevo83@gmail.com>
2023-12-05 10:03:42 -0500

    Add files via upload

--- 2023AdventDay1.py
+++ 2023AdventDay1.py
@@ -1,13 +1,12 @@
+replacements = [("one","one1one"),("two","two2two"),("three","three3three"),("four","four4four"),("five","five5five"),("six","six6six"),("seven","seven7seven"),("eight","eight8eight"),("nine","nine9nine")]
 with open('Calibration document.txt') as f:
     alllines = []
     for line in f.readlines():
+        for old, new in replacements:
+            line = line.replace(old, new)
         nums = []
-        for char in line:
-            if char.isnumeric():
-                nums.append(char)
-        #print(nums)
+        [nums.append(char) for char in line if char.isnumeric()]
         linetot = nums[0]+nums[-1]
-        #print(linetot)
         alllines.append(linetot)
     allnums = [int(x) for x in alllines]
     alltot = sum(allnums)
